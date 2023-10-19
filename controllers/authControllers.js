0f33bbb87433cf1af03fbaccd81f63bbc50fef57
Umair513 <soomroumair513@gmail.com>
2023-10-19 23:21:22 +0500

    Login API

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs")
 const userModel = require("../models/userModel")
+const jwt = require("jsonwebtoken")
 
 const registerController = async (req, res) => {
     try {
@@ -33,4 +34,37 @@ const registerController = async (req, res) => {
     }
 }
 
-module.exports = { registerController }
+
+const loginController = async (req, res) => {
+    try {
+        const user = await userModel.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "user not found"
+            })
+        }
+        const comparePassword = await bcrypt.compare(req.body.password, user.password)
+        if (!comparePassword) {
+            return res.status(500).send({
+                success: false,
+                message: "Invalid credentials"
+            })
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
+        return res.status(200).send({
+            success: true,
+            message: "Login successfully",
+            token,
+            user
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            message: "Error in Login Api",
+            error
+        })
+    }
+}
+module.exports = { registerController, loginController }
