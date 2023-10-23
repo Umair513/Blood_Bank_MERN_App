2e4997ee544e88bf59ea43791acaed3255693a1d
Umair513 <soomroumair513@gmail.com>
2023-10-23 23:59:59 +0500

    Login form

---
+++ client/src/components/shared/form/InputTypes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const InputTypes = ({ value, onChange, name, inputType, labelText, labelFor }) => {
+    return (
+        <>
+            <div className="mb-3">
+                <label htmlFor={labelFor} className="form-label">{labelText}</label>
+                <input type={inputType} className="form-control" name={name} value={value} onChange={onChange} />
+            </div>
+        </>
+    )
+}
+
+export default InputTypes
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import InputTypes from '../../components/shared/form/InputTypes'
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
+        <>
+            <div className="row">
+                <div className="col-md-8 form-banner">
+                    <img src="./assets/images/banner1.jpg" alt='LoginImage'></img>
+                </div>
+                <div className="col-md-4 form-container">
+                    <form>
+                        <InputTypes labelText={"E-mail"} labelFor={"forE-mail"} inputType={"email"} name={"email"}></InputTypes>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+
+                </div>
+            </div>
+        </>
     )
 }
 
